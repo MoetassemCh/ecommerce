50584eaeb2a8fe5bc11a6b54e4393e22fa9c5b6c
MoetassemCh <chebbomoe@gmail.com>
2025-08-06 14:49:43 +0300

    adding signin and signup and validation middleware

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const db = require('./config/db');
+const multer = require('multer');
 // const productRoutes = require("./routes/productRoutes");
-// const authRoutes = require("./routes/authRoutes");
+const authRoutes = require("./routes/authRoutes");
 // const orderRoutes = require("./routes/orderRoutes.js");
 // const cartRoutes = require("./routes/cartRoutes");
 const {notFound, errorHandler} = require('./middleware/errorMiddleware');
@@ -19,13 +20,18 @@ app.use(express.json());
 // Parse incoming URL-encoded data
 app.use(express.urlencoded({ extended: true }));
 
+// Multer setup for file uploads
+const upload = multer();
+// Test route
+
+
  app.get('/', (req, res) => {
     res.send('API is running...');
 });
 
 // Routes
 // app.use('/api/products', productRoutes);
-// app.use('/api/auth', authRoutes);
+app.use('/api/auth', upload.none(), authRoutes);
 // app.use('/api/order', orderRoutes);
 // app.use('/api/cart', cartRoutes);
 
